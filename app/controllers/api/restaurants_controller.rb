c7fb11cb28018c15130fcc00bd5fa248f5c52d9b
mellowjoeys <mellowjoey@gmail.com>
2019-10-07 17:04:54 -0500

    added Restaurant info still need to seed/create associations

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,2 +1,32 @@
 class Api::RestaurantsController < ApplicationController
+
+  def index
+    # response = HTTP['X-RapidAPI-Host': "us-restaurant-menus.p.rapidapi.com", 'X-RapidAPI-Key': "af3fb5beb8mshe060fab2244aa4ap1d1e9fjsnbe098ae6aecf"].get("https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=-87.634466&lat=41.892543&distance=.25")
+    # unformatted_restaurants = response.parse["result"]["data"]
+    # @restaurants =  []
+    # unformatted_restaurants.each do |restaurant|
+    #   Restaurant.create()
+    #   #
+    # end
+    # @restaurants = Restaurant.all
+    # @restaurants.each
+
+    render 'index.json.jb'
+  end
+
+  # def create
+  #   @restaurant = Restaurant.new(
+  #                                 name: 
+  #                                 address:
+  #                                 cuisine:
+  #                                 hours:
+  #                                 )
+  # end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    render 'show.json.jb'
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+response = HTTP['X-RapidAPI-Host': "us-restaurant-menus.p.rapidapi.com", 'X-RapidAPI-Key': "af3fb5beb8mshe060fab2244aa4ap1d1e9fjsnbe098ae6aecf"].get("https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=-87.634466&lat=41.892543&distance=.25")
+unformatted_restaurants = response.parse["result"]["data"]
+unformatted_restaurants.each do |restaurant|
+  Restaurant.create(restaurant.) # have to make Restaurant, only adding restaurant attributes. Can add dishes to restaurant after this. 
+  #
+end
+@restaurants = Restaurant.all
+@restaurants.each
