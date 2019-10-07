061a79192a3e03dbda4a5201d0c072323938b409
mellowjoeys <mellowjoey@gmail.com>
2019-10-07 14:00:48 -0500

    added Restaurant id to dish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_040031) do
+ActiveRecord::Schema.define(version: 2019_10_07_184116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_040031) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
   end
 
   create_table "menus", force: :cascade do |t|
